b5436acae5f892f416dbda36e190322bb1f153ab
shanuka2000 <115928935+shanuka2000@users.noreply.github.com>
2023-03-04 19:19:46 +0530

    initial commit

---
+++ src/main/java/com/example/buynowpaylater/BuyNowPayLaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.buynowpaylater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyNowPayLaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyNowPayLaterApplication.class, args);
+	}
+
+}
